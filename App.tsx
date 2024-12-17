cd3f151f5c97bab91d0e44c0fd2c39ac39502b6e
Aqil G <aqilgifari0@gmail.com>
2024-12-17 13:43:28 +0700

    feat: animated example

--- App.tsx
+++ App.tsx
@@ -1,12 +1,14 @@
 import { ScrollView, StyleSheet } from "react-native";
 import BasicComponent from "./src/components/BasicComponent";
 import Touches from "./src/components/Touches";
+import AnimatedExample from "./src/components/AnimatedExample";
 
 export default function App() {
   return (
     <ScrollView style={styles.container} showsVerticalScrollIndicator={false}>
       <Touches text="coba coba" />
       <BasicComponent />
+      <AnimatedExample />
     </ScrollView>
   );
 }
