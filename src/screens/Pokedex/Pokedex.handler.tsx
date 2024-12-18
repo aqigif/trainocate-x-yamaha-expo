1764ba5f582878130a4ac3dfe1d59464895a0846
Aqil G <aqilgifari0@gmail.com>
2024-12-18 15:09:18 +0700

    fix: import

--- src/screens/Pokedex/Pokedex.handler.tsx
+++ src/screens/Pokedex/Pokedex.handler.tsx
@@ -1,4 +1,4 @@
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { pokemons } from "../../data/pokemons";
 import { addToMyPoke } from "../../redux/actions/myPokeAction";
 import { useAppSelector } from "../../hooks/useReduxType";
